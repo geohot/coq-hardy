7b539369bb000e58b8ca93f80851e7f7cd224193
George Hotz <geohot@gmail.com>
2018-12-18 15:40:56 -0500

    we have proved a lemma

--- chapter_2.v
+++ chapter_2.v
@@ -4,9 +4,8 @@ Require Export Zpow_facts.
 
 Definition inf_primes_of_form (f : Z -> Z) :=
   forall l : Z, exists n : Z, l < n /\ prime (f n).
+  
 
-  
-  
 (* Theorem 10: x>=2 -> pi(x) >= loglog x *)
 
 (* Theorem 11: Infinitely many primes of form, 4n+3 *)
@@ -39,8 +38,49 @@ Proof.
 Abort.
 
 (* Theorem 16: No two Fermat numbers have a gcd > 1 *)
-Theorem theorem_16 : forall n k : Z,
-    rel_prime ((2^(2^n)) + 1) ((2^(2^(n+k))) + 1).
+Definition F (n : Z) := 2^2^n + 1.
+
+Lemma div_minus_trans : forall m a b : Z, (m|a) /\ (m|(a+b)) -> (m | b).
 Proof.
   intros.
+  destruct H.
+  destruct H.
+  destruct H0.
+  rewrite H in H0.
+
+  assert (mdb : x0 * m - x * m = b). omega.
+  clear H0 H a.
+  rewrite <- Z.mul_sub_distr_r in mdb.
+
+  exists (x0 - x).
+  auto.
+Qed.
+
+Theorem theorem_16 : forall n k : Z,
+    rel_prime (F n) (F (n+k)).
+Proof.
+  intros.
+  constructor; auto with zarith.
+  intros m dn dnk.
+
+  assert (mn2 : Z.abs m <> 2).
+  admit.
+  
+  assert ((F n)|((F (n+k))-2)).
+  admit.
+
+  specialize (Z.divide_trans m (F n) (F (n + k) - 2)).
+  intros.
+  apply H0 in H; [|assumption].
+  clear H0 dn.
+
+  specialize (div_minus_trans m (F (n + k)) (-2)).
+  intros.
+  destruct H0.
+  split; auto.
+
+  clear H dnk.
+  
+  
 Abort.
+
