d4b5d33b0e2bd2da9ba2e0a8de37752e8198ab76
George Hotz <geohot@gmail.com>
2018-12-20 11:15:43 -0500

    starting on 70

--- chapter_6.v
+++ chapter_6.v
@@ -2,10 +2,40 @@ Require Export ZArith.
 Require Export Znumtheory.
 Require Export Zpow_facts.
 
+Check Z.pow_pos.
+Search (0 ^ _).
+
 (* Theorem 70 *)
-Theorem theorem_70 : forall p : Z, prime p -> forall a : Z, a^p mod p = a mod p.
+Lemma freshmans_dream : forall p : Z, prime p -> forall x y : Z, (x + y)^p mod p = (x^p+y^p) mod p.
 Proof.
-Abort.
+Admitted.
+
+Theorem theorem_70 : forall p : Z, prime p -> forall a : Z, a>=0 -> a^p mod p = a mod p.
+Proof.
+  intros.
+  pattern a.
+  apply natlike_ind.
+  rewrite Z.pow_0_l.
+  reflexivity.
+  apply prime_ge_2 in H.
+  omega.
+
+  intros.
+  replace (Z.succ x) with (x + 1); [|omega].
+  rewrite freshmans_dream.
+  replace (1^p) with 1.
+  rewrite <- Zplus_mod_idemp_l.
+  rewrite H2.
+  rewrite -> Zplus_mod_idemp_l.
+  reflexivity.
+
+  symmetry.
+  apply Z.pow_1_l.
+  apply prime_ge_2 in H.
+  omega.
+  assumption.
+  omega.
+Qed.
 
 (* Theorem 71 *)
 Theorem fermats_theorem : forall a p : Z, prime p /\ ~(p|a) -> a^(p-1) mod p = 1.
@@ -15,5 +45,3 @@ Abort.
 (* Theorem 72 : a^phi(m) mod m = 1 *)
 
 
-
-  
