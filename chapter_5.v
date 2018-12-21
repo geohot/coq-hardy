b583f7b3cc2d8a2fa822ea96d8da48f2f2de8dce
George Hotz <geohot@gmail.com>
2018-12-21 10:43:24 -0500

    more ish

--- chapter_5.v
+++ chapter_5.v
@@ -3,11 +3,28 @@ Require Export Znumtheory.
 Require Export Zpow_facts.
 
 (* Theorem 54 *)
+Lemma mod_sub_ish : forall a b m : Z, a mod m = b mod m -> (a - b) mod m = 0.
+Proof.
+  intros.
+Admitted.           
+
 Theorem theorem_54 :
   forall k m d : Z, Zis_gcd k m d ->
                     forall a a' : Z, k*a mod m = k*a' mod m -> a mod (m/d) = a' mod (m/d).
 Proof.
   intros.
+  induction H.
+  destruct H as [k1 H].
+  destruct H1 as [m1 H1].
+
+  assert (rel_prime k1 m1).
+  constructor.
+  exists k1; omega.
+  exists m1; omega.
+  intros.
+  admit.  
+
+  
 Admitted.
 
 (* Theorem 55 *)
