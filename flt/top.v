d61d5f5e76dd10845fa73bf750b8d9df1766e394
George Hotz <geohot@gmail.com>
2018-12-24 14:35:56 -0500

    wow i'm bad at ssreflect

--- flt/top.v
+++ flt/top.v
@@ -3,8 +3,115 @@ Require Import Coq.omega.Omega.
 
 (* see http://www.cs.rug.nl/~wim/fermat/wilesEnglish.html for proof sketch *)
 
-Theorem theorem_1 : forall n, (exists a b c, a^n + b^n = c^n) -> (exists a b c, a^n + b^n = c^n /\ (coprime a b)).
+Search (_ %| _).
+
+Lemma gcdmultk k a b : gcdn (k*a) (k*b) == k -> gcdn a b == 1.
+Proof. 
+  move/eqP.
+  
+
+  intros.
+  
+  
+  intros gk.
+  apply/eqP/gcdn_def; try by [].
+  intros.
+  
+Admitted.
+
+Lemma coprime_gcd a b : coprime (a %/ (gcdn a b)) (b %/ (gcdn a b)).
 Proof.
+  unfold coprime.
+  apply (gcdmultk (gcdn a b)).
+  
+  
+  
+
+  
+  
+  
+  
+  apply/eqP.
+  apply gcdn_def; try by [].
+  intros.
+  
+  
+  intros d'.
+  
+  move=> /dvdnP.
+  intros ek.
+  destruct ek.
+  
+  move=> /dvdnP.
+  intros el.
+  destruct ek.
+  destruct el.
+  
+  
+  
+  intros.
+
+  case/dvdnP=> n'.
+  
+  rewrite/dvdnP in H.
+  
+
+  
+  apply coprime_dvdl with (n:=a).
+  unfold coprime.
+  
+  assert (gabda : gcdn a b %| a).
+  apply dvdn_gcdl.
+
+  assert (gabdb : gcdn a b %| b).
+  apply dvdn_gcdr.
+
+  apply/eqP.
+  
+  apply gcdn_def; try by [].
+  intros.
+  
+  
+
+ 
+  admit.
+
+  
+  
+
+
+
+  
+  intros.
+  
+  
+Admitted.
+
+Search ((_ %/ _) ^ _).
+
+
+Theorem theorem_1 : forall n, (exists x y z, x^n + y^n = z^n) -> (exists a b c, a^n + b^n = c^n /\ (coprime a b)).
+Proof.
+  intros n [x [y [z H]]].
+  pose (d := gcdn x y).
+
+  (* assert (ddivx : d %| x). *)
+  
+  exists (x %/ d).
+  exists (y %/ d).
+  exists (z %/ d).
+  split.
+  
+  2 : {
+    unfold d.
+    apply coprime_gcd.
+  }
+  
+
+
+  
+  
+  
 Admitted.
 
 Theorem theorem_2 : forall a b c, a^4 + b^4 <> c^4.
@@ -35,7 +142,16 @@ Proof.
     red; intros; rewrite H0 in eq; rewrite eq in H; contradiction.
   }
   assert (ngt4 : n > 4). {
-    admit.
+    (* ugh ssreflect is annoying *)
+    rewrite ltn_neqAle ltn_neqAle.
+    case and3P.
+    intros; by [].
+    intros.
+    destruct n0.
+    split.
+    apply/eqnP; auto.
+    apply/eqnP; auto.
+    assumption.
   }
   clear ngt nn4 nn3.
 
@@ -56,5 +172,8 @@ Proof.
   clear a b c eq.
   rename x into a.
   destruct H as [b [c [eq coab]]].
+
+  (* and here some stuff about elliptic curves *)
 Abort.
-  
+
+
