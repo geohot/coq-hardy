9581d0c3486a81b3ea177abbf50e7661632a6c58
George Hotz <geohot@gmail.com>
2018-12-25 06:36:43 -0500

    even better

--- chapter_4.v
+++ chapter_4.v
@@ -5,51 +5,45 @@ Theorem sqrt2_irrational a b : a>0 -> a*a <> 2*b*b.
 Proof.
   red.
   generalize b.
-
-  elim a using (well_founded_ind lt_wf).
+  pattern a.
+  apply (well_founded_ind lt_wf).
   clear.
   intros a H b anz eq.
 
-  assert (Nat.Even (a * a)).
+  assert (a2e : Nat.Even (a * a)).
   exists (b*b).
   rewrite -> Nat.mul_assoc.
   assumption.
 
-  assert (Nat.Even a).
+  assert (ae : Nat.Even a).
   rewrite <- even_equiv.
-  rewrite <- even_equiv in H0.
-  apply even_mult_aux in H0.
-  now destruct H0.
+  rewrite <- even_equiv in a2e.
+  apply even_mult_aux in a2e.
+  now destruct a2e.
   
   assert (bnz : b > 0).
   specialize (gt_0_eq b).
   intros [|]; [assumption|].
-  rewrite <- H2 in eq.
+  rewrite <- H0 in eq.
   simpl in eq.
   apply Nat.eq_mul_0 in eq.
   destruct eq; omega.
  
-  destruct H1.
+  destruct ae as [x a2x].
   specialize (H b).
   destruct H with (b0 := x).
-  - symmetry in eq.
-    apply Nat.square_lt_simpl_nonneg; [omega|].
-    rewrite <- eq.
+  - apply Nat.square_lt_simpl_nonneg; [omega|].
+    rewrite -> eq.
     rewrite <- Nat.mul_assoc.
     rewrite <- (Nat.mul_1_l (b * b)) at 1.
-    apply Nat.mul_lt_mono_pos_r.
+    apply Nat.mul_lt_mono_pos_r; [|omega].
     apply Nat.mul_pos_pos; omega.
-    omega.    
   - assumption.
-  - rewrite H1 in eq.
+  - rewrite a2x in eq.
     repeat rewrite <- Nat.mul_assoc in eq.
     rewrite -> Nat.mul_cancel_l in eq; auto.
     rewrite Nat.mul_comm in eq.
-    rewrite eq.
-    reflexivity.
+    symmetry.
+    assumption.
 Qed.
 
-
-  
-  
-  
