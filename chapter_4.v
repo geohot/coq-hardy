2555c231c04b5941f9bb1eece9ebc04699490881
George Hotz <geohot@gmail.com>
2018-12-25 13:31:46 -0500

    theorem 46

--- chapter_4.v
+++ chapter_4.v
@@ -1,4 +1,4 @@
-Require Import Omega Wf_nat Even.
+Require Import Omega Wf_nat Even Classical_Pred_Type.
 
 (* Theorem 43: sqrt(2) is irrational *)
 Theorem sqrt2_irrational a b : a>0 -> a*a <> 2*b*b.
@@ -47,12 +47,68 @@ Proof.
     assumption.
 Qed.
 
+Search (_ * (_ / _)).
+
 (* Theorem 46: log10(2) is irrational *)
-Theorem log102_irrational a b : a > 0 -> 2^b <> 10^a.
+
+Lemma fivex_2b x b : 5*x <> 2^b.
+Proof. 
+  generalize x.
+  clear x.
+  
+  induction b.
+  red.
+  intros.
+  simpl in H.
+  omega.
+  
+  red.
+  intros.
+  assert (Nat.Even x).
+  rewrite <- even_equiv.
+  apply even_mult_inv_r with (n:=5).
+  rewrite -> H.
+  rewrite Nat.pow_succ_r.
+  rewrite -> even_equiv.
+  exists (2^b).
+  reflexivity.
+  omega.
+  rewrite -> odd_equiv.
+  exists 2.
+  auto.
+
+  destruct H0.
+  specialize (IHb x0).
+  destruct IHb.
+  rewrite Nat.pow_succ_r in H; [|omega].
+
+  rewrite <- (Nat.mul_cancel_l _ _ 2); [|omega].
+  rewrite <- H.
+  rewrite -> H0.
+  repeat rewrite Nat.mul_assoc.
+  auto.
+Qed.
+
+Search (S (pred _)).
+
+Theorem log102_irrational a b : (a > 0) -> (2^b <> 10^a).
 Proof.
   red.
-  intros anz H.
-Abort.
+  intros anz eq.
+
+  assert (exists c, 5*c = 10^a).
+  exists (2 * (10^(pred a))).
+  rewrite Nat.mul_assoc.
+  replace (5 * 2) with 10; [|omega].
+  rewrite <- Nat.pow_succ_r; [|omega].
+  rewrite <- S_pred_pos; [|omega].
+  reflexivity.
+  
+  destruct H.
+  rewrite <- eq in H.
+  apply fivex_2b in H.
+  assumption.
+Qed.
 
 (* Theorem 47: e is irrational *)
 
